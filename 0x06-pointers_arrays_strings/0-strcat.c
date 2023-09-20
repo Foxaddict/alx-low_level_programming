d6a3cdb31485af1d6a02905e2aa67db7a25acc86
foxaddict <felix4u.ca@gmail.com>
2023-09-20 10:33:23 -0700

    0-strcat.c

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+ * _strcat - Brings two string together
+ * @dest: add value
+ * @src: second value
+ *
+ * Return: void
+ */
+char *_strcat(char *dest, char *src)
+{
+	int i;
+	int k;
+
+	i = 0;
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
+	k = 0;
+	while (src[k] != '\0')
+	{
+		dest[i] = src[k];
+		i++;
+		k++;
+	}
+
+	dest[i] = '\0';
+	return (dest);
+}
+
